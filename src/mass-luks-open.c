894e98d04b9bb74abd42aad782916acc5a4a47ab
Anthony Lieuallen <arantius@gmail.com>
2025-01-01 09:54:43 -0500

    volumes are opened

--- src/mass-luks-open.c
+++ src/mass-luks-open.c
@@ -16,25 +16,24 @@ You should have received a copy of the GNU General Public License along
 with this program; if not, see <https://www.gnu.org/licenses/>.
 */
 
-#include <search.h>
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 #include <unistd.h>
 
 #include <blkid/blkid.h>
-//#include <libcryptsetup.h>
+#include <bsd/readpassphrase.h>
+#include <libcryptsetup.h>
 
 
-#define VOL_STATUS_UNKNOWN 0
-#define VOL_STATUS_NEW 1
+#define VOL_STATUS_UNKNOWN 1
 #define VOL_STATUS_OPEN 2
 #define VOL_STATUS_FAIL 3
 
 
 struct LuksVolume {
-  const char* device_name;
+  const char* device_path;
   const char* label;
   int status;
 
@@ -42,9 +41,9 @@ struct LuksVolume {
 };
 
 
-struct LuksVolume* _new_luks_volume(const char* device_name, const char* label) {
+struct LuksVolume* _new_luks_volume(const char* device_path, const char* label) {
   struct LuksVolume* v = malloc(sizeof(struct LuksVolume));
-  v->device_name = strdup(device_name);
+  v->device_path = strdup(device_path);
   v->label = strdup(label);
   v->status = VOL_STATUS_UNKNOWN;
   v->next = NULL;
@@ -55,7 +54,7 @@ struct LuksVolume* _new_luks_volume(const char* device_name, const char* label) 
 int gather_luks_volumes(struct LuksVolume** list) {
   blkid_probe pr;
   blkid_dev dev;
-  const char *device_name;
+  const char *device_path;
   const char *label;
   const char *type;
 
@@ -78,18 +77,21 @@ int gather_luks_volumes(struct LuksVolume** list) {
   struct LuksVolume* tail = NULL;
 
   while (!blkid_dev_next(iter, &dev)) {
-    device_name = blkid_dev_devname(dev);
-    if (!device_name) continue;
+    device_path = blkid_dev_devname(dev);
+    if (!device_path) continue;
 
-    type = blkid_get_tag_value(cache, "TYPE", device_name);
+    type = blkid_get_tag_value(cache, "TYPE", device_path);
     if (!type || strcmp(type, "crypto_LUKS") != 0) continue;
 
-    label = blkid_get_tag_value(cache, "PARTLABEL", device_name);
+    label = blkid_get_tag_value(cache, "PARTLABEL", device_path);
     if (!label) {
-      label = blkid_get_tag_value(cache, "LABEL", device_name);
+      label = blkid_get_tag_value(cache, "LABEL", device_path);
+    }
+    if (!label) {
+      label = blkid_get_tag_value(cache, "UUID", device_path);
     }
 
-    struct LuksVolume* volume = _new_luks_volume(device_name, label);
+    struct LuksVolume* volume = _new_luks_volume(device_path, label);
     if (*list == NULL) {
       *list = volume;
       tail = volume;
@@ -107,6 +109,84 @@ int gather_luks_volumes(struct LuksVolume** list) {
 }
 
 
+int open_luks_volumes(struct LuksVolume** volumes) {
+  #define PASSPHRASE_MAX_LEN 512
+  char* passphrase = crypt_safe_alloc(PASSPHRASE_MAX_LEN);
+  size_t passphraseLen = 0;
+  struct LuksVolume* v = *volumes;
+  while (v != NULL) {
+    if (v->status == VOL_STATUS_OPEN) continue;
+
+    if (passphraseLen == 0) {
+      if (readpassphrase(
+          "Enter LUKS decryption passphrase: ", passphrase,
+          PASSPHRASE_MAX_LEN, RPP_REQUIRE_TTY) == NULL) {
+        fprintf(stderr, "readpassphrase() failed.\n");
+        goto check_drives;
+      }
+      passphraseLen = strlen(passphrase);
+    }
+
+    printf(
+        "Opening LUKS volume [%s] with label [%s]... ",
+        v->device_path, v->label);
+
+    struct crypt_device *cd;
+    if (crypt_init(&cd, v->device_path)) {
+      fprintf(stderr, "crypt_init() failed for %s.\n", v->device_path);
+      return 1;
+    }
+
+    if (crypt_load(cd, CRYPT_LUKS, NULL)) {
+      fprintf(stderr, "crypt_load() failed for %s.\n", v->device_path);
+      goto next_drive_fail;
+    }
+
+    int r = crypt_activate_by_passphrase(
+        cd, v->label, CRYPT_ANY_SLOT, passphrase, passphraseLen,
+        CRYPT_ACTIVATE_ALLOW_DISCARDS);
+    if (r < 0) {
+      printf("Device %s activation failed.\n", v->device_path);
+      goto next_drive_fail;
+    }
+
+    printf("success.\n");
+    v->status = VOL_STATUS_OPEN;
+    goto next_drive;
+
+    next_drive_fail:
+    v->status = VOL_STATUS_FAIL;
+
+    next_drive:
+    crypt_free(cd);
+    v = v->next;
+  }
+
+  check_drives:
+  crypt_safe_free(passphrase);
+
+  int num_open = 0;
+  int num_fail = 0;
+  v = *volumes;
+  while (v != NULL) {
+    if (v->status == VOL_STATUS_OPEN) num_open++; else num_fail++;
+    v = v->next;
+  }
+  if (num_fail > 0) {
+    char input[2];
+    printf(
+        "Successfully opened %d, failed to open %d.  Retry? [Y/n] ",
+        num_open, num_fail);
+    fgets(input, sizeof(input), stdin);
+    if (input[0] == '\n' || input[0] == 'Y' || input[0] == 'y') {
+      return open_luks_volumes(volumes);
+    }
+  }
+
+  return 0;
+}
+
+
 int main(int argc, char **argv) {
   if (geteuid()) {
     printf("mass-luks-open requires super user privileges.\n");
@@ -115,12 +195,11 @@ int main(int argc, char **argv) {
 
   struct LuksVolume* volumes = NULL;
   if (gather_luks_volumes(&volumes)) return 1;
-  
-  struct LuksVolume* v = volumes;
-  while (v != NULL) {
-    printf("LUKS volume [%s] has label [%s].\n", v->device_name, v->label);
-    v = v->next;
+  if (volumes == NULL) {
+    fprintf(stderr, "Found no LUKS volumes!\n");
+    return 1;
   }
+  if (open_luks_volumes(&volumes)) return 1;
 
   return 0;
 }
